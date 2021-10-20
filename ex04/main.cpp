417e018e47bb81dcc3640184b9e25fdf7995dfd7
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-20 16:10:36 +0300

    [rf][split] +checkArgs()

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,21 +2,52 @@
 #include <fstream>
 #include <iostream>
 
-int main(int, char *argv[])
+namespace ft
 {
-	if (!argv[1] || !argv[2] || !argv[3] || !argv[2][0] || !argv[3][0]) {
-		std::cerr << "Wrong args. Usage:" << std::endl;
-		std::cerr << argv[0] << " file string_to_replace string_to_replace_with"
-		          << std::endl;
-		return 1;
+	void replace(std::string& line, const char* s)
+	{
+		std::string line2;
+		{
+			size_t	pos = 0;
+			size_t 	replace_pos = line.find(s);
+			line2 = line.substr(0, replace_pos);
+			if (replace_pos < line.length())
+				line2.append(s);
+			pos = replace_pos;
+
+		}
 	}
-	std::ifstream	fIn(argv[1]);
+}
+bool checkArgs(int argc, char* const argv[])
+{
+	const char* s1 = argv[2];
+	const char* s2 = argv[3];
+
+	if (argc < 4 || !s1[0] || !s2[0]) {
+		std::cerr << "Wrong args" << std::endl;
+		std::cout << "Usage:" << std::endl;
+		std::cout << argv[0] << " file string_to_replace string_to_replace_with"
+		          << std::endl;
+		return false;
+	}
+	return true;
+}
+int		main(int argc, char* argv[])
+{
+	if (!checkArgs(argc, argv))
+		return 1;
+	std::string		inFileName(argv[1]);
+	std::ifstream	fIn(inFileName);
 	if (!fIn) {
 		std::cerr << "Bad file" << std::endl;
 		return 2;
 	}
-	std::string		fName(argv[1]);
-	std::ofstream 	fOut(fName.append(".replace"));
+	std::ofstream 	fOut(inFileName.append(".replace"));
+	if (!fOut) {
+		std::cerr << "Failed to create/open "
+		          << inFileName << ".replace" << std::endl;
+		return 3;
+	}
 	std::string 	line;
 	while(fIn) {
 		std::getline(fIn, line);
